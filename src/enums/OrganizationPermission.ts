9eca555c462be51d94ec2f1ae4b1624f45c5ec09
Mukhtar Kiboigo <muki@kiboigo.com>
2023-09-11 15:49:50 -0700

    fix: organization permission enum

--- src/enums/OrganizationPermission.ts
+++ src/enums/OrganizationPermission.ts
@@ -1,4 +1,4 @@
-export enum LapStatus {
+export enum OrganizationPermission {
 	OWNER = 'OWNER',
 	OFFICER = 'OFFICER',
 	NONE = 'NONE'
--- src/enums/all.ts
+++ src/enums/all.ts
@@ -1,5 +1,6 @@
 export * from './EventStatus';
 export * from './LapFlag';
 export * from './LapStatus';
+export * from './OrganizationPermission';
 export * from './RequestMethod';
 export * from './VisibilityStatus';
